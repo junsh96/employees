0f28139826a399f1bc682cae1f7d6b1aa8e155b0
JEON <junsh96@naver.com>
2019-09-20 17:39:16 +0900

    controller.getDepartmentsListServlet생성

---
+++ employees/src/controller/GetDepartmentsListServlet.java
@@ -0,0 +1,29 @@
+package controller;
+import model.*;
+import vo.*;
+import java.util.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GetDepartmentsListServlet
+ */
+@WebServlet("/departments/getDepartmentsList")
+public class GetDepartmentsListServlet extends HttpServlet {
+	private DepartmentsDao departmentsDao;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		departmentsDao = new DepartmentsDao();
+		List<Departments> list = departmentsDao.selectDepartmentsList();
+		
+		request.setAttribute("list", list);
+		
+		request.getRequestDispatcher("/WEB-INF/views/departments/departmentsList.jsp").forward(request, response);
+	}
+	
+
+}
