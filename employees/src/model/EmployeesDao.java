a47a7145d15ac37d135fa7ceb34e8ca9ed1498e2
JEON <junsh96@naver.com>
2019-09-23 17:27:24 +0900

    사원들 리스트를 이름순으로 오름차순, 내림차순으로 출력하기위한 코드와 쿼리문 추가

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -10,6 +10,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeesDao {
+	
+	public List<Employees> selectEmployeesListOrderBy(String order){
+		System.out.println("selectEmployeesListOrderBy param order :" + order);
+		List<Employees> list = new ArrayList<Employees>();
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		
+		if(order.equals("asc")) {
+			sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees order by first_name asc limit 50";
+		} else if(order.equals("desc")) {
+			sql = "select emp_no, birth_date, first_name, last_name, gender, hire_date from employees order by first_name desc limit 50";
+		}
+			try {
+				Class.forName("org.mariadb.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/employees","root","java1234");
+				stmt = conn.prepareStatement(sql);
+				rs = stmt.executeQuery();
+				while(rs.next()) {
+					Employees employees = new Employees();
+					employees.setEmpNo(rs.getInt("emp_no"));
+					employees.setBirthDate(rs.getString("birth_date"));
+					employees.setFirstName(rs.getString("first_name"));
+					employees.setLastName(rs.getString("last_name"));
+					employees.setGender(rs.getString("gender"));
+					employees.setHireDate(rs.getString("hire_date"));
+					list.add(employees);
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					rs.close();
+					stmt.close();
+					conn.close();
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return list;
+	}
+	
 	public List<Employees> selectEmplyeesList(int limit) {
 		System.out.println("EmployeesDao selectEmployeesListByLimit parama limit :"+limit);
 		List<Employees> list = new ArrayList<Employees>();
@@ -21,7 +65,7 @@ public class EmployeesDao {
 		
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees","root","wkqk1234");
+			conn=DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees","root","java1234");
 			stmt=conn.prepareStatement(sql);
 			stmt.setInt(1,limit);
 			rs=stmt.executeQuery();
@@ -64,7 +108,7 @@ public class EmployeesDao {
 		try {
 			//드라이브 이름
 			Class.forName("org.mariadb.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees","root","wkqk1234");
+			conn=DriverManager.getConnection("jdbc:mariadb://localhost:3306/employees","root","java1234");
 			stmt=conn.prepareStatement(sql);
 			rs=stmt.executeQuery();
 			if(rs.next()) {
