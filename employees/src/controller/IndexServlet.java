c4fc68ea7d3d780b822ada1ec19c880ddc826e89
JEON <junsh96@naver.com>
2019-09-23 17:24:28 +0900

    (deptManager,dept_emp,titles,salaries)Dao를 index.jsp에서 실행하기 위해 indexServlet.java에 코드 추가

--- employees/src/controller/IndexServlet.java
+++ employees/src/controller/IndexServlet.java
@@ -11,11 +11,32 @@ import model.*;
 @WebServlet({"/","/index"})
 public class IndexServlet extends HttpServlet {
 	private EmployeesDao employeesDao;
+	private DepartmentsDao departmentsDao;
+	private DeptManagerDao deptManagerDao;
+	private EmpDeptDao empDeptDao;
+	private TitlesDao titlesDao;
+	private SalariesDao salariesDao; 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		employeesDao = new EmployeesDao();
+		departmentsDao = new DepartmentsDao();
+		deptManagerDao = new DeptManagerDao();
+		int deptManagerRowCount =deptManagerDao.selectDeptManagerRowCount(); 
+		empDeptDao = new EmpDeptDao();
+		int empDeptRowCount = empDeptDao.selectDeptRowCount(); 
+		titlesDao = new TitlesDao();
+		int titlesRowCount = titlesDao.selectTitlesRowCount(); 
+		salariesDao = new SalariesDao();
+		int salariesRowCount = salariesDao.selectSalariesRowCount();
+		int departmentsRowCount = departmentsDao.selectDepartMentsRowCount(); 
 		int employeesRowCount = employeesDao.selectEmployeesRowCount();
 		
+		request.setAttribute("deptManagerRowCount", deptManagerRowCount);
+		request.setAttribute("empDeptRowCount", empDeptRowCount);
+		request.setAttribute("salariesRowCount", salariesRowCount);
+		request.setAttribute("titlesRowCount", titlesRowCount);
 		request.setAttribute("employeesRowCount", employeesRowCount);
+		request.setAttribute("departmentsRowCount", departmentsRowCount);
 		
 		request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
 	}
