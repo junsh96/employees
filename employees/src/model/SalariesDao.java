172df31c76dd49dc7ef3d1bb4fe342cd7630ee1a
JEON <junsh96@naver.com>
2019-09-24 11:47:17 +0900

    salariesDao에 총연봉액, 평균액수,최대치, 최소치등을 출력하는 코드 추가

--- employees/src/model/SalariesDao.java
+++ employees/src/model/SalariesDao.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import DB.DBHelper;
+import java.util.*;
 public class SalariesDao {
 	public int selectSalariesRowCount() {
 		int count = 0;
@@ -38,5 +39,41 @@ public class SalariesDao {
 		
 		return  count;
 	}
+	public Map<String, Long> selectSalariesStatistics(){
+		Map<String, Long> map = new HashMap<String, Long>();
+		
+		String sql="SELECT COUNT(salary),SUM(salary),AVG(salary),MAX(salary),MIN(salary),STD(salary) FROM salaries";
+		Connection conn =null;
+		PreparedStatement stmt=null;
+		ResultSet rs = null;
+		try {
+			//드라이브 이름
+			conn = DBHelper.getConnection();
+			stmt=conn.prepareStatement(sql);
+			rs=stmt.executeQuery();
+			while(rs.next()) {
+				map.put("count",rs.getLong("COUNT(salary)"));
+				map.put("sum",rs.getLong("SUM(salary)"));
+				map.put("avg",rs.getLong("AVG(salary)"));
+				map.put("max",rs.getLong("MAX(salary)"));
+				map.put("min",rs.getLong("MIN(salary)"));
+				map.put("std",rs.getLong("STD(salary)"));
+			}
+			
+		}catch(Exception e) { // 자바의 변수 생명주기는 {} 
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
+	
 }
 
