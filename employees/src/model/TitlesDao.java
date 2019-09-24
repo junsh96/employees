198207e5ab1312325b10a4eca9f0e927b339ca14
JEON <junsh96@naver.com>
2019-09-24 11:46:28 +0900

    titlesDao에 직업리스트를 출력하는 코드 추가

--- employees/src/model/TitlesDao.java
+++ employees/src/model/TitlesDao.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import java.util.*;
+import DB.DBHelper;
 public class TitlesDao {
 	public int selectTitlesRowCount() {
 		int count = 0;
@@ -38,6 +39,31 @@ public class TitlesDao {
 		
 		return  count;
 	}
+	public List<String> selectTitlesListDistinct() {
+		List<String> list = new ArrayList<String>();
+		String sql="SELECT DISTINCT title FROM titles";
+		Connection conn =null;
+		PreparedStatement stmt=null;
+		ResultSet rs = null;
+		try {
+			//드라이브 이름
+			conn = DBHelper.getConnection();
+			stmt=conn.prepareStatement(sql);
+			rs=stmt.executeQuery();
+			while(rs.next()) {
+				list.add(rs.getString("title"));
+			}
+			
+		}catch(Exception e) { // 자바의 변수 생명주기는 {} 
+			e.printStackTrace();
+		}finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+	
+		
+		return list;
+	}
+	
 }
 
 
