288dc92933529fe7ce77e2d7e8968d4472bc09dc
JEON <junsh96@naver.com>
2019-09-24 11:47:56 +0900

    TitlesDao의 서블릿 추가

---
+++ employees/src/controller/GetTitlesListDistinctServlet.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.TitlesDao;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+/**
+ * Servlet implementation class GetTitlesListDistinctServlet
+ */
+@WebServlet("/titles/getTitlesListDistinct")
+public class GetTitlesListDistinctServlet extends HttpServlet {
+	private TitlesDao titlesDao;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		titlesDao = new TitlesDao();
+		List<String> list = titlesDao.selectTitlesListDistinct();
+		request.setAttribute("list", list);
+		request.getRequestDispatcher( "/WEB-INF/views/titles/titlesListDistinct.jsp").forward(request, response);
+	}
+
+}
