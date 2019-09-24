5c1702060ad310d7047bfc18d3b66bbc97b3fbae
JEON <junsh96@naver.com>
2019-09-24 18:10:23 +0900

    성별 사원수, 사원번호 검색용 코드 추가

--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -11,7 +11,69 @@ import java.util.List;
 
 import DB.DBHelper;
 import DB.DBHelper;
+import java.util.*;
 public class EmployeesDao {
+	
+	public List<Map<String, Object>> selectEmployeesCountGroupByGender(){
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql ="SELECT gender, COUNT(gender) as cnt FROM employees GROUP BY gender";
+		try {
+			conn = DBHelper.getConnection();
+			stmt=conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("gender" , rs.getString("gender"));
+				map.put("cnt", rs.getInt("cnt"));
+				list.add(map);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		return list;
+	}
+	
+	public List<Employees> selectEmployeesListBetween(int begin, int end){
+		List<Employees> list = new ArrayList<Employees>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql="SELECT emp_no,birth_date,first_name,last_name,gender,hire_date FROM employees WHERE emp_no BETWEEN ? AND ? ORDER BY emp_no ASC";
+		
+		
+		try {
+			conn = DBHelper.getConnection();
+			stmt=conn.prepareStatement(sql);
+			stmt.setInt(1,begin);
+			stmt.setInt(2,end);
+			rs=stmt.executeQuery();
+			
+			while(rs.next()) {
+				Employees employees = new Employees();
+				 employees.setEmpNo(rs.getInt("emp_no"));
+				 employees.setBirthDate(rs.getString("birth_date" ));
+				 employees.setFirstName(rs.getString("first_name" ));
+				 employees.setLastName(rs.getString("last_name" ));
+				 employees.setGender(rs.getString("gender" ));
+				 employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);	
+			}
+			
+		}catch(Exception e) {
+			
+		}finally {
+			DBHelper.close(rs, stmt, conn);
+		}
+		
+		
+		return list;
+	}
+	
 	
 	public List<Employees> selectEmployeesListOrderBy(String order){
 		System.out.println("selectEmployeesListOrderBy param order :" + order);
